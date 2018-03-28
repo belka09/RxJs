43296cc012f662e4ae9f18202b96c8b5fcbaf5e9
dzimnitsky <dzimnitsky@dev-team.com>
2018-03-28 22:03:32 +0300

    learn about methods of, range, timer

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-let button = document.querySelector('button');
-let btn$ = Rx.Observable.fromEvent(button, 'click');
+function createSubscribe(name) {
+ return {
+     next(x) {
+         console.log(name, ": ", x);
+     },
+    error(err){
+        console.log("Error: " , err)
+    },
+    complete(){
+        console.log(name, " :completed");
+    }
+ }
+}
 
-btn$.subscribe(function (e) {
-    console.log(e);
-});
+// Rx.Observable.of(5, 4, 52, 8, "lol", "kek", [4, 25, 25, 25, 9])
+//     .subscribe(createSubscribe('of'));
 
-Rx.Observable.fromEvent(document.querySelector('input'), 'keyup')
-    .subscribe(e => {
-        console.log(e);
-    });
+// Rx.Observable.interval(500)
+//     .take(15)
+//     .subscribe(createSubscribe('interval'));
 
-Rx.Observable.fromEvent(document, 'mousemove')
-    .subscribe(e => {
-        document.querySelector('h1').innerHTML = `X: ${e.clientX} , Y: ${e.clientY}`;
-    });
+// Rx.Observable.timer(4000, 500)
+//     .take(10)
+//     .subscribe(createSubscribe('timer'));
+
+Rx.Observable.range(5, 15)
+    .subscribe(createSubscribe('range'));
