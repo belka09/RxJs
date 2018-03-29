e638f1e53d93a821081a495f086e3137a007adf3
dzimnitsky <dzimnitsky@dev-team.com>
2018-03-29 12:58:17 +0300

    learn method "from" in RxJs. Training from with Map and Set methods

--- index.js
+++ index.js
@@ -23,5 +23,12 @@ function createSubscribe(name) {
 //     .take(10)
 //     .subscribe(createSubscribe('timer'));
 
-Rx.Observable.range(5, 15)
-    .subscribe(createSubscribe('range'));
+// Rx.Observable.range(3, 6)
+//     .subscribe(createSubscribe('range'));
+
+// const set = new Set([1, 2, 3, '4' , {id: 6}, 1, 8, 3]);
+
+const map = new Map([[1, 2], [3, 4], [5,6]])
+ 
+Rx.Observable.from(map)
+    .subscribe(createSubscribe('from'));
