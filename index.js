c107efd0e8331f81b3ab7775628dde6dd8330be1
Dmitry Zimnitsky <dmitryft4@gmail.com>
2018-03-31 10:30:16 +0300

    learn methods pluck and map

--- index.js
+++ index.js
@@ -28,7 +28,19 @@ function createSubscribe(name) {
 
 // const set = new Set([1, 2, 3, '4' , {id: 6}, 1, 8, 3]);
 
-const map = new Map([[1, 2], [3, 4], [5,6]])
+// const map = new Map([[1, 2], [3, 4], [5,6]])
  
-Rx.Observable.from(map)
-    .subscribe(createSubscribe('from'));
+// Rx.Observable.from(map)
+//     .subscribe(createSubscribe('from'));
+
+Rx.Observable.fromEvent(document.querySelector('input'), 'keyup')
+    // .map(x => x.target.value)
+    .pluck('target', 'value')
+    .map(x => x.toUpperCase())
+    .map(x => {
+        return {
+            value: x,
+            length: x.length
+        };
+    })
+    .subscribe(createSubscribe('map'));
